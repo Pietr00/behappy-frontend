2f5bbae9879304cd00051838d37e157fe35e716a
Pietr00 <nalbertpietro@gmail.com>
2018-10-26 17:30:56 -0300

    complexos e dominio

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import "./css/pure.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
